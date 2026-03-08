50b8fa87afd1e0afb6c9949a88cdb27338f0fb14
General-Monish <monissam28@gmail.com>
2026-03-08 22:48:53 +0530

    Learning Fragments-Topic:

---
+++ learning_Fragments-Topic/src/App.jsx
@@ -0,0 +1,19 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Playstaion Games Lists</h1>
+      <ul class="list-group">
+        <li class="list-group-item">God Of War</li>
+        <li class="list-group-item">Uncharted</li>
+        <li class="list-group-item">The Last Of Us</li>
+        <li class="list-group-item">Horizon Zero Dawn</li>
+        <li class="list-group-item">Spider-Man</li>
+      </ul>
+    </React.Fragment>
+  );
+}
+
+export default App;

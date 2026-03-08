6c596274a9c9832d9b6eaf89849d3d9dc771b100
General-Monish <monissam28@gmail.com>
2026-03-08 14:35:10 +0530

    started working on to do app, added some basic structure and functionality to the app.jsx file. I will continue to work on it and add more features as I go along. and now will define the components and the state management for the app. I will also work on the styling of the app to make it look better. I am excited to see how this app will turn out and I hope it will be useful for people who want to keep track of their tasks and to-dos.

--- To-do-app-version-1/src/App.jsx
+++ To-do-app-version-1/src/App.jsx
@@ -1,8 +1,44 @@
-
 function App() {
-  return <center class="todo-container">
-    <h1>To-Do App</h1>
-  </center>
+  return (
+    <center class="todo-container">
+      <h1>To-Do App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter To Do Here" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-outline-success">Add</button>
+          </div>
+        </div>
+                <div class="row">
+          <div class="col-6">
+            Buy Playstation game
+          </div>
+          <div class="col-4">
+            25/02/2026
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-outline-danger">Danger</button>
+          </div>
+        </div>
+                <div class="row">
+          <div class="col-6">
+            Buy Playstation games soon
+          </div>
+          <div class="col-4">
+            25/02/2026
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-outline-danger">Danger</button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;

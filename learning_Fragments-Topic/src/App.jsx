c5ee6f06335b8ae8d9211bee1dc6671196c79536
General-Monish <monissam28@gmail.com>
2026-03-08 23:55:46 +0530

    done with map method and fragments topic. Now commiting the changes to git.

--- learning_Fragments-Topic/src/App.jsx
+++ learning_Fragments-Topic/src/App.jsx
@@ -1,18 +1,28 @@
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 function App() {
+      let games = ["God Of War", "Uncharted", "The Last Of Us", "Horizon Zero Dawn", "Spider-Man"];
   return (
-    <React.Fragment>
+    // <React.Fragment>
+    //   <h1>Playstaion Games Lists</h1>
+    //   <ul class="list-group">
+    //     <li class="list-group-item">God Of War</li>
+    //     <li class="list-group-item">Uncharted</li>
+    //     <li class="list-group-item">The Last Of Us</li>
+    //     <li class="list-group-item">Horizon Zero Dawn</li>
+    //     <li class="list-group-item">Spider-Man</li>
+    //   </ul>
+    // </React.Fragment>
+    <>
       <h1>Playstaion Games Lists</h1>
-      <ul class="list-group">
-        <li class="list-group-item">God Of War</li>
-        <li class="list-group-item">Uncharted</li>
-        <li class="list-group-item">The Last Of Us</li>
-        <li class="list-group-item">Horizon Zero Dawn</li>
-        <li class="list-group-item">Spider-Man</li>
+      <ul className="list-group">
+        {games.map((game) => (
+          <li key={game} className="list-group-item">
+            {game}
+          </li>
+        ))}
       </ul>
-    </React.Fragment>
+    </>
   );
 }
 

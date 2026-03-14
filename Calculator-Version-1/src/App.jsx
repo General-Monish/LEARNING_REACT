0fb80bdbb14a773ba5d74144da24e2519221c0d3
General-Monish <monissam28@gmail.com>
2026-03-14 14:02:57 +0530

    Done with basic sturucture of Calculator project

--- Calculator-Version-1/src/App.jsx
+++ Calculator-Version-1/src/App.jsx
@@ -1,12 +1,16 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./Components/App.module.css";
+
 function App() {
   return (
     <>
-      <div>
-        <center>
-          <h1>CAlculator</h1>
-        </center>
+      <div className={styles.calculator}>
+        <input className={styles.input_display} type="text" />
+        <div className={styles.btn_container}>
+          <button className={styles.btn}>C</button>
+          <button className={styles.btn}>1</button>
+          <button className={styles.btn}>2</button>
+          <button className={styles.btn}>+</button>
+        </div>
       </div>
     </>
   );

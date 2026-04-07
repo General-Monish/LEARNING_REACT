1ec6d4ee9d38371f369171ddc7a8279ca411b6ae
General-Monish <monissam28@gmail.com>
2026-04-08 00:35:42 +0530

    Added a buy button to the games component and added a click event handler to it which will show an alert when the button is clicked. Also added some styling to the button to make it look better.

--- learning_Fragments-Topic/src/Components/Games.jsx
+++ learning_Fragments-Topic/src/Components/Games.jsx
@@ -1,10 +1,20 @@
 import styles from "./Games.module.css";
 
 const Games = (props) => {
+  const BuyButtonClicked = (event) => {
+    alert(`You have bought ${props.games}`);
+  };
   return (
     // ${styles["playstation-games"]} --> this is our way to use the styles from the css module file, we can also use styles.playstation-games but for now i am keeping it outside of the className to show that we can use it in combination with other classes as well
     <li className={`list-group-item`}>
       {props.games}
+      <button
+        className={`${styles["buyButton"]} btn btn-success`}
+        // here event handler is added to the button, when the button is clicked it will call the BuyButtonClicked function which will show an alert with the name of the game that was bought.Also we are passing the event object to the function so that we can use it to get the value of the button that was clicked.
+        onClick={(event) => BuyButtonClicked(event)}
+      >
+        BUY
+      </button>
     </li>
   );
 };

78a6a9cf228916122838d9ed5f947c6bdd5b3931
General-Monish <monissam28@gmail.com>
2026-04-08 23:56:39 +0530

    added: learning_Fragments-Topic/src/Components/GamesInput.jsx added: learning_Fragments-Topic/src/Components/GamesInput.module.css modified: learning_Fragments-Topic/src/App.jsx added input component for games and updated App.jsx to include it

--- learning_Fragments-Topic/src/App.jsx
+++ learning_Fragments-Topic/src/App.jsx
@@ -2,6 +2,7 @@ import ErrorMessage from "./Components/ErrorMessage";
 import PlayStationGames from "./Components/PlayStationGames";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "./Components/Container";
+import GamesInput from "./Components/GamesInput";
 function App() {
   // const games=[];
   const games = [
@@ -43,6 +44,7 @@ function App() {
       {/* conditionally rendering the error message if there are no games available in the games array and also rendering the list of games using the PlayStationGames component. */}
       <ErrorMessage gamesItems={games} />
       {/* gamesItems prop is passed to the PlayStationGames component, which will use it to render the list of games. If the games array is empty, the ErrorMessage component will display a message indicating that no games are available. */}
+      <GamesInput />
       <PlayStationGames gamesItems={games} />
     </Container>
   );
---
+++ learning_Fragments-Topic/src/Components/GamesInput.jsx
@@ -0,0 +1,5 @@
+import styles from "./GamesInput.module.css";
+const GamesInput = () => {
+    return <input type="text" className={styles["games-input"]} />
+}
+export default GamesInput;

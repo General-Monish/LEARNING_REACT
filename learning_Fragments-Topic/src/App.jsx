5df3bb03e07bb1461b5f1bfd05393ff3d9c936e7
General-Monish <monissam28@gmail.com>
2026-04-10 15:43:14 +0530

    We are going to use the states now to store the value entered by the user in the input field and then display it on the screen. We will also use the useState hook to create a state variable called gameEnteredByUser and a function to update it called setGameEnteredByUser. We will then pass the handleOnChange function as a prop to the GamesInput component, which will call it whenever the user types something in the input field. The handleOnChange function will update the gameEnteredByUser state variable with the value entered by the user, and we will display it on the screen using a paragraph element.

--- learning_Fragments-Topic/src/App.jsx
+++ learning_Fragments-Topic/src/App.jsx
@@ -13,32 +13,11 @@ function App() {
     "Spider-Man",
     "Ghost of Tsushima",
   ];
-      const handleOnChange = (event)=>{
-        console.log(event.target.value);
-    }
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+  };
+  let gameEnteredByUser = "Seikro";
   return (
-    //                               fragment
-    // <React.Fragment>
-    //   <h1>PlayStation Games Lists</h1>
-    //   <ul class="list-group">
-    //     <li class="list-group-item">God Of War</li>
-    //     <li class="list-group-item">Uncharted</li>
-    //     <li class="list-group-item">The Last Of Us</li>
-    //     <li class="list-group-item">Horizon Zero Dawn</li>
-    //     <li class="list-group-item">Spider-Man</li>
-    //   </ul>
-    // </React.Fragment>
-    //                                  MAP Method
-    // <>
-    //   <h1>PlayStation Games Lists</h1>
-    //   <ul className="list-group">
-    //     {games.map((game) => (
-    //       <li key={game} className="list-group-item">
-    //         {game}
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </>
     //                                                         PROPS and CONDITIONAL RENDERING
     <Container>
       <h1 style={{ fontFamily: "Arial", fontWeight: 800 }}>
@@ -48,6 +27,7 @@ function App() {
       <ErrorMessage gamesItems={games} />
       {/* gamesItems prop is passed to the PlayStationGames component, which will use it to render the list of games. If the games array is empty, the ErrorMessage component will display a message indicating that no games are available. */}
       <GamesInput handleOnChange={handleOnChange} />
+      <p>{gameEnteredByUser}</p>
       <PlayStationGames gamesItems={games} />
     </Container>
   );
